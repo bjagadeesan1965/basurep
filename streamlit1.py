6f807db5949bb72b1d7645fc0c95c7ea2e27dc2c
bjagadeesan1965 <147236428+bjagadeesan1965@users.noreply.github.com>
2023-10-18 18:31:53 -0700

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -21,7 +21,7 @@ def load_data():
     with st.spinner(text="Loading and indexing the Streamlit docs – please wait. This should take a few minutes."):
         reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
         docs = reader.load_data()
-        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features."))
+        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=1.0, system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features."))
         index = VectorStoreIndex.from_documents(docs, service_context=service_context)
         return index
 
